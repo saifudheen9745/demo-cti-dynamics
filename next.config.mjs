f94467b7ce164778167a4221fcb9bb998132251c
saifudheen-sg <saifudheen.m@sysgrate.com>
2025-02-21 19:27:52 +0530

    updaated the config file next js

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     async headers() {
-      return [
-        {
-          source: '/:path*',
-          headers: [
-            {
-              key: 'Content-Security-Policy',
-              value: "frame-ancestors 'self' https://*.dynamics.com https://*.crm.dynamics.com https://*.dynamics365.com *"
-            },
-            {
-              key: 'Access-Control-Allow-Origin',
-              value: '*'
-            }
-          ],
-        },
-      ];
-    },
-  }
-  
-  module.exports = nextConfig
+        return [
+          {
+            source: '/:path*',
+            headers: [
+              {
+                key: 'Content-Security-Policy',
+                value: "frame-ancestors 'self' https://*.dynamics.com https://*.crm.dynamics.com https://*.dynamics365.com *"
+              },
+              {
+                key: 'Access-Control-Allow-Origin',
+                value: '*'
+              }
+            ],
+          },
+        ];
+      },
+};
+
+export default nextConfig;
