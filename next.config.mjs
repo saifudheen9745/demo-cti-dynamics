13291887806f8feb3b104a3840f4e4a6147ac335
saifudheen-sg <saifudheen.m@sysgrate.com>
2025-02-21 19:07:14 +0530

    updated the config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: '/:path*',
+          headers: [
+            {
+              key: 'X-Frame-Options',
+              value: 'ALLOWALL'
+            },
+            {
+              key: 'Content-Security-Policy',
+              value: "frame-ancestors *;" // Allows all domains
+            },
+            {
+              key: 'Access-Control-Allow-Origin',
+              value: '*' // Allows all origins
+            }
+          ],
+        },
+      ];
+    },
+  }
+  
 
 export default nextConfig;
